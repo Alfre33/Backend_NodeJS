7b7926b86e89af4247753e17c121603767d214ce
Alfre33 <josegaspr28383@gmail.com>
2023-09-19 19:53:10 -0600

    Actualizacion de servidor

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo sere el backend!');
+  res.send('¡Hola, mundo sere el backend por ahora!');
 });
 
 app.listen(port, () => {
